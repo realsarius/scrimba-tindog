31f70ce6f558344fe80f57c749ea7d354ca7c7de
realsarius <berkansozer@protonmail.com>
2023-08-01 17:36:21 +0300

    fixed empty object calling a function

--- index.js
+++ index.js
@@ -4,28 +4,37 @@ import Dog from "./Dog.js";
 
 document.addEventListener("click", (e) => {
   if (e.target.id === "crossBtn" || e.target.id === "crossImg") {
-    dog.getCrossed();
-    dog = getNewDog();
-    render();
+    if (Object.keys(dog).length !== 0) {
+      dog.getCrossed();
+      setTimeout(() => {
+        dog = getNewDog();
+        render();
+      }, 1500);
+    }
   }
 
   if (e.target.id === "heartBtn" || e.target.id === "heartImg") {
-    console.log("heart");
+    if (Object.keys(dog).length !== 0) {
+      dog.getLiked();
+      setTimeout(() => {
+        dog = getNewDog();
+        render();
+      }, 1500);
+    }
   }
 });
 
 const getNewDog = () => {
   const newDog = dogs.length != 0 ? new Dog(dogs.shift()) : {};
-  return newDog;
+  return Object.keys(newDog).length !== 0 ? newDog : {};
 };
 
 const render = () => {
-  document.querySelector("#dogMain").innerHTML = dog.getDogHtml();
+  if (Object.keys(dog).length !== 0) {
+    document.querySelector("#dogMain").innerHTML = dog.getDogHtml();
+  }
 };
 
 let dog = getNewDog();
 
 render();
-
-// const crossBtn = document.querySelector("#crossBtn");
-// const heartBtn = document.querySelector("#heartBtn");
